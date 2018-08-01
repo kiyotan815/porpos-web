1353c304916123aaa1d358fefd402d88b9491c0d
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-08-01 17:53:21 +0900

    fix routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  get 'users/new'
-
   root 'top_pages#home'
   get '/rules',   to: 'top_pages#rules'
   get '/privacy', to: 'top_pages#privacy'
   get '/porpos',  to: 'top_pages#index'
+  
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    get '/logout', to: 'devise/sessions#destroy'
+    get '/signup',  to: 'devise/registrations#new'
+  end
+  devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

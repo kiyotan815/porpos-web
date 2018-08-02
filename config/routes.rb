a12c42a00eeec8c9c16c6707339bf230ffeedab9
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-08-02 16:45:09 +0900

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
 
   root 'top_pages#home'
   get '/rules',   to: 'top_pages#rules'
   get '/privacy', to: 'top_pages#privacy'
   get '/porpos',  to: 'top_pages#index'
+  get '/signup',  to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20180802073040) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "pssword_digest"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

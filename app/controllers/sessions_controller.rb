98d4a5e3c6ad65cb3e1903340d5fb1c7ded57563
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-08-02 17:55:34 +0900

    fix css

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,18 @@
 class SessionsController < ApplicationController
+
   def new
-   
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash[:danger] = 'メールアドレス、またはパスワードが一致しません' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end

840ce6f80060537091a14ed2a5d7b19e5ee91f79
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-08-02 17:43:14 +0900

    add signup & login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ログインが完了しました"
+      redirect_to @user  
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,4 +27,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   
 
-  root 'top_pages#home'
-  get '/rules',   to: 'top_pages#rules'
-  get '/privacy', to: 'top_pages#privacy'
-  get '/porpos',  to: 'top_pages#index'
-  get '/signup',  to: 'users#new'
+  get 'sessions/new'
+
+  root   'top_pages#home'
+  get    '/rules',    to: 'top_pages#rules'
+  get    '/privacy',  to: 'top_pages#privacy'
+  get    '/porpos',   to: 'top_pages#index'
+  get    '/signup',   to: 'users#new'
+  post   '/signup',   to: 'users#create'
+  get    '/login',    to: 'sessions#new'
+  post   '/login',    to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

6bbfa9bb629825fff6f8821851820783762a8645
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-08-06 14:47:04 +0900

    fix css

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -6,7 +6,7 @@ class TopPagesController < ApplicationController
   def index
     @portfolios = Portfolio.all
     @arrival = Portfolio.limit(4).order("created_at DESC")
-    @portfolio_all = Portfolio.all.order("created_at DESC")
+    @portfolio_all = Portfolio.all
   end
 
   def rules

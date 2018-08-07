979be140a93ed4e4739852ea8b0fe2ef132b40c1
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-08-07 11:54:22 +0900

    fix css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :show]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:destroy, :index]
 
   def index
     @users = User.all
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
       flash[:success] = "登録が完了しました"
-      redirect_to @user  
+      redirect_to @user
     else
       render 'new'
     end
@@ -42,11 +42,11 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  def show 
+  def show
     @user = User.find(params[:id])
     @portfolios = @user.portfolios.all
   end
-  
+
   private
 
   def user_params

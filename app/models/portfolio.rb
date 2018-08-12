de24190220462fba9b928653a1dddb529b26e836
kiyota <kyt.iabragi0815@gmail.com>
2018-08-12 21:58:31 +0900

    initialize

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -25,6 +25,7 @@
 
 class Portfolio < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   mount_uploader :catcheye_img, ImageUploader
   validates :user_id, presence: true
   validates :title, presence:true, length: { maximum: 100 }

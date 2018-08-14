60a5654ae45469f812f3059be73d7e742bb54865
kiyota <kyt.iabragi0815@gmail.com>
2018-08-14 17:58:29 +0900

    ok

--- db/migrate/20180812125030_create_comments.rb
+++ db/migrate/20180812125030_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.text :comment
       t.references :portfolio, foreign_key: true
-
+      t.references :user, foreign_key:true
       t.timestamps
     end
     add_index :comments, [:portfolio_id, :created_at]

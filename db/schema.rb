e9e98c2bf3f363c50062650eb57a06288a226b12
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-08-21 09:30:48 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180813053242) do
+ActiveRecord::Schema.define(version: 20180812152415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20180813053242) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.bigint "portfolio_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.index ["portfolio_id", "created_at"], name: "index_comments_on_portfolio_id_and_created_at"
     t.index ["portfolio_id"], name: "index_comments_on_portfolio_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -50,13 +50,10 @@ ActiveRecord::Schema.define(version: 20180813053242) do
     t.boolean "admin", default: false
     t.string "icon_img"
     t.string "uuid", null: false
-    t.bigint "portfolio_id"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["portfolio_id"], name: "index_users_on_portfolio_id"
   end
 
   add_foreign_key "comments", "portfolios"
   add_foreign_key "comments", "users"
   add_foreign_key "portfolios", "users"
-  add_foreign_key "users", "portfolios"
 end
